c2109695a0794e431164e9eec4db0d5262e5b62e
Jack Knox <jackjackfilms1@gmail.com>
2026-06-20 10:44:55 +0000

    Change vector types from union to struct

--- protocol/ember/core/math_types.h
+++ protocol/ember/core/math_types.h
@@ -3,26 +3,26 @@
 #include "ember/core.h"
 
 // A 2-element vector.
-typedef union vec2 {
+typedef struct vec2 {
     f32 x, y;
 } vec2;
 
 // A 3-element vector.
-typedef union vec3 {
+typedef struct vec3 {
     f32 x, y, z;
 } vec3;
 
 // A 4-element vector.
-typedef union vec4 {
+typedef struct vec4 {
     float x, y, z, w;
 } vec4;
 
 // A unsigned integer 2-element vector.
-typedef union uvec2 {
+typedef struct uvec2 {
     u32 x, y;
 } uvec2;
 
 // A unsigned interger 3-element vector.
-typedef union uvec3 {
+typedef struct uvec3 {
     u32 x, y, z;
 } uvec3;
